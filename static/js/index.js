5dbcdb75420e226f4fd56ffc2193be2e1c9d9cb4
Alexander Kaufmann <akaufmann038@gmail.com>
2021-05-20 16:58:27 -0400

    added ship movement

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,9 @@ const message_input = document.getElementById("message")
 const messageLog_div = document.getElementById("message-log-container")
 const outerContainer_div = document.getElementById("container-div")
 const container_body = document.getElementById("body-container")
+
+keyDown = { 87: false, 65: false, 83: false, 68: false }
+
 // NOTE: when deploying, take argument out of io
 //"http://127.0.0.1:5000/chat"
 var socket = io("http://127.0.0.1:5000/chat")
@@ -27,9 +30,10 @@ function launchShip(text) {
         ship.style.width = "10%"
         ship.style.height = "10%"
         ship.style.position = "absolute"
-        ship.style.left = "100px"
+        ship.style.left = "350.55px"
         ship.style.top = "200px"
         ship.style.zIndex = "5"
+        ship.style.transform = "rotate(0deg)"
 
         document.documentElement.appendChild(ship)
 
@@ -60,9 +64,7 @@ socket.on('message', function (msg) {
     var userName = document.createTextNode(msg.slice(0, nameEnd + 1))
 
     // append necessary elements
-    boldName.appendChild(userName)
-    newP.appendChild(boldName)
-    newP.appendChild(text)
+    newP.innerHTML = "<strong>" + msg.slice(0, nameEnd + 1) + "</strong> " + msg.slice(nameEnd + 1)
 
     messageContainer.appendChild(newP)
     container_div.appendChild(messageContainer)
@@ -95,28 +97,121 @@ message_input.addEventListener("keyup", function (event) {
     }
 });
 
-function update(progress) {
-    
-}
 
 // add event listener for w key
 container_body.addEventListener("keydown", function (event) {
-    if (document.activeElement !== message_input) {
-        if (event.keyCode == 87) {
-            var ship = document.getElementById("ship")
-            var currentLeft = ship.style.left
-            var numLeft = parseInt(currentLeft.substring(0, currentLeft.search("p")))
+    // w: 87, a: 65, s: 83, d: 68
 
-            ship.style.left = (numLeft + 5) + "px"
-            console.log(ship.style.left)
+
+    if (document.activeElement !== message_input) {
+        if (event.keyCode in keyDown) {
+            keyDown[event.keyCode] = true
         }
     }
 })
 
+container_body.addEventListener("keyup", function (event) {
+    if (event.keyCode in keyDown) {
+        keyDown[event.keyCode] = false
+    }
+})
+
+// resets all of the keyDown values to false
+function resetKeyDown() {
+    Object.keys(keyDown).forEach(element => {
+        keyDown[element] = false
+    })
+}
+function moveShip(key) {
+    if (parseInt(key) == 87) {
+        var ship = document.getElementById("ship")
+
+        // get ship angle
+        var angle = parseInt(ship.style.transform.substring(7, ship.style.transform.search("d")))
+        //console.log(ship.style.transform)
+
+        // get x and y scalars from rotation
+        var xScalar = 1.5 * Math.sin(toRadians(angle))
+        var yScalar = 1.5 * Math.cos(toRadians(angle))
+
+        // change ships location
+        changeLocation(xScalar, yScalar)
+
+
+
+        // console.log("move up")
+        // var currentTopStyle = ship.style.top
+        // var currentTopNum = parseInt(currentTopStyle.substring(0, currentTopStyle.search("p")))
+        // console.log(currentTopNum)
+
+        // // change top style
+        // ship.style.top = currentTopNum + 1 + "px"
+
+        // console.log(ship.style.top)
+    }
+    else if (key == 65) {
+        changeDegree(-1)
+    }
+    else if (key == 68) {
+        changeDegree(1)
+    }
+}
+
+function changeLocation(xScalar, yScalar) {
+    //console.log("x: " + xScalar + " y: " + yScalar)
+    var ship = document.getElementById("ship")
+
+    // get current x and y
+    var currentX = parseFloat(ship.style.left.substring(0, ship.style.left.search("p")))
+    var currentY = parseFloat(ship.style.top.substring(0, ship.style.top.search("p")))
+
+    // update x and y
+    ship.style.left = currentX + xScalar + "px"
+    ship.style.top = currentY - yScalar + "px"
+    //console.log("new x: " + currentX + xScalar + " new y: " + currentY + yScalar)
+}
+
+function toRadians (angle) {
+    return angle * (Math.PI / 180);
+  }
+
+// changes the rotation of the ship element
+function changeDegree(diff) {
+    var ship = document.getElementById("ship")
+
+    // get current degree
+    var currentDegreeStyle = ship.style.transform
+    var currentDegreeNum = parseInt(currentDegreeStyle.substring(7, currentDegreeStyle.search("d")))
+
+    // change style
+    if (currentDegreeNum == 0 && diff == -1) {
+        var newDegree = 359
+    }
+    else if (currentDegreeNum == 359 && diff == 1) {
+        var newDegree = 0
+    }
+    else {
+        var newDegree = currentDegreeNum + diff
+    }
+    ship.style.transform = "rotate(" + newDegree + "deg)"
+}
+
+// updates state of game based on keyDown
+function updateGame() {
+
+    // based on current keyDowns, move ship appropriately
+    Object.keys(keyDown).forEach(element => {
+        // if key is held down
+        if (keyDown[element]) {
+            moveShip(element)
+        }
+    })
+}
 
 function gameLoop() {
-    console.log(performance.now())
+    //console.log(performance.now())
+    // update state of game
+    updateGame()
 
-    
-    setTimeout(gameLoop, 10)
+    setTimeout(gameLoop, 1)
 }
